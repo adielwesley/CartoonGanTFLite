8bd270d466ba4ef9cf6733939df7c87d3f688336
Adiel Silva <adielws@motorola.com>
2022-05-05 14:56:18 -0400

    Create template for ML Binding

---
+++ app/src/main/java/com/adielwesley/cartoongan/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.adielwesley.cartoongan
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import com.adielwesley.cartoongan.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var original: Bitmap? = null
+
+    // To do: create model variable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loadOriginalButton.setOnClickListener { onLoadOriginalImageClicked() }
+        binding.cartoonizeButton.setOnClickListener { onCartoonizeClicked() }
+
+        // To do: initialize model
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // To do: release memory
+    }
+
+    private fun onCartoonizeClicked() {
+        original?.let {
+            // To do: prepare input
+
+            // To do: run model
+
+            // To do: handle output
+        }
+    }
+
+    private fun onLoadOriginalImageClicked() {
+        getContent.launch("image/*")
+    }
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        binding.originalImageView.setImageURI(it)
+        original = binding.originalImageView.drawable.toBitmap()
+    }
+}
